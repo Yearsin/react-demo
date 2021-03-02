63530999f6cdc98c8f50185cadd3865f422c2b67
qilicang <18303033133@163.com>
2021-03-02 20:22:26 +0800

    't'

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        this.home()
+    }
+    home = () => {
+        console.log(this.api.basic);
+        // this.get(`${this.api.home}`, '').then( res => {
+        //     console.log(res)
+        // })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Home页面</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
